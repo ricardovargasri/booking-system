3dde78187d379792b96d7c60e110f5ed0883a892
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2026-03-27 21:35:38 -0500

    mejoras en la implementacion del service

--- src/main/java/com/booking_1/demo/services/userService/UserServiceImpl.java
+++ src/main/java/com/booking_1/demo/services/userService/UserServiceImpl.java
@@ -1,5 +1,45 @@
 package com.booking_1.demo.services.userService;
 
-public class UserService {
+import com.booking_1.demo.dtos.UserDto;
+import com.booking_1.demo.dtos.UserRegistrationDto;
+import com.booking_1.demo.entities.User;
+import com.booking_1.demo.mappers.UserMapper;
+import com.booking_1.demo.repositories.UserRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDto save(UserRegistrationDto userRegistrationDto) {
+
+        User user = UserMapper.toEntity(userRegistrationDto);
+        User userSaved = userRepositoru.save(user);
+
+        return UserMapper.toDto(userSaved);
+    }
+
+    public UserDto findById(UUID id) {
+        return userRepository.findById(id)
+                .map(user -> UserMapper.toDto(user))
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+    }
+
+    public UserDto updateUser(UUID id, UserRegistrationDto userRegistrationDto) {
+
+    }
+
+    public void deleteUser(UUID id) {
+
+    }
 }
