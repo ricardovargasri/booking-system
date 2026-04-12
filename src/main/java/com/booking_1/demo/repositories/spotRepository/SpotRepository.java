26b5fbd6bccb18a4eb01f4423437ffaf7bee81d5
Ricardo Vargas <rvargas@nexura.com>
2026-04-12 14:48:37 -0500

    branch with some errors

--- src/main/java/com/booking_1/demo/repositories/spotRepository/SpotRepository.java
+++
@@ -1,8 +0,0 @@
-package com.booking_1.demo.repositories.spotRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.booking_1.demo.spot.entities.Spot;
-
-public interface SpotRepository extends JpaRepository<Spot, Long> {
-}
